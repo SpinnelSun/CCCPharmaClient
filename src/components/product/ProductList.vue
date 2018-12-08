e78477fb2db4a572d2210c5f8ac3a46ce18d368d
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-08 10:18:06 -0300

    Create ProductList
    Just for tests, needs a lot of adjusts

---
+++ src/components/product/ProductList.vue
@@ -0,0 +1,43 @@
+<template id = "list">
+    <div id="app" class="columns">
+        <div class="column">
+            <ul>
+                <li id="component"> <product-component v-for="product in products" v-bind:product="product"> </product-component> </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ProductComponent from '@/components/product/ProductComponent'
+    
+
+    export default {
+        name: 'ProductList',
+        data() {
+            return {
+                products : [{name: "camisinha", price: 2.75},{  name: "camisinha",price: 2.85},{ name: "camisinha",price: 2.85}]
+            }
+        },
+        components: {
+            ProductComponent
+        }
+    }
+
+</script>
+
+<style>
+    .column ul{
+        padding:0px;
+        margin:0px;
+        background-color:#EDEDED;
+        list-style:none;
+    }
+
+    #component {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    
+</style>
+
