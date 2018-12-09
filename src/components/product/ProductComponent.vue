401a760e7eb4dc723bb06544a1415a4857575f4e
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-09 00:38:29 -0300

    Revert "Add CSS to list"
    This reverts commit d05acf1806e6faede81ff6f0caad86c5067700b3.

--- src/components/product/ProductComponent.vue
+++ src/components/product/ProductComponent.vue
@@ -12,19 +12,17 @@
           <div class="media-content">
 
             <div class="content">
-                <h3 id="product-name"> {{product.name}} </h3> 
-
-                <span :class="{available: product.avaliable, unavailable: !product.available}">
-                  <i class="fa fa-warning"></i>
-                    INDISPONÍVEL
-                </span>
-
-                <h2 id="product-price"> R$ {{product.price}} </h2>
+              <p>
+                <strong>{{product.name}}</strong> <small>{{product.price}}</small>
+              </p>
             </div>
-            <div class="admin-options">
-              <a id="clickable" href="#">EDITAR</a>
-              <a id="clickable" href="#">VER</a>
-            </div>
+
+              <div class="level-left">
+                <a class="level-item">
+                  <button class="clicavel" >VER</button>
+                  <button class="clicavel">EDITAR</button>
+                </a>
+              </div>
           </div>
         </article>
 
@@ -42,12 +40,6 @@ export default {
 </script>
 
 <style>
-  .unavailable{
-    color: #ffae00;
-  }
-  .available{
-    color: rgb(154, 231, 227);
-  }
   #product-component template{
   height: 100%;
   padding-top: 10px;
@@ -67,52 +59,19 @@ export default {
 #product-component .product {
   max-width: 500px;
   margin: 0 auto;
-  padding-bottom: 5px;
-}
-
-#clickable{
-  text-decoration: none;
-  margin-right: 5px;
-  margin-top: 0px;
-}
-
-.media-content{
-  padding: 0px;
-  margin: 0px;
-}
-
-.admin-options{
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  text-decoration: none;
-  text-align: left;
-}
-
-#product-name{
-  text-align: left;
-  color: darkblue;
-  margin: 0px;
-}
-
-#product-situation{
-  text-align: left;
-  padding: 0px;
-}
-
-#product-price{
-  text-align: right;
-  color: rgb(243, 60, 60);
+  padding-bottom: 15px;
 }
 
 #product-component .box {
   width: 300px;
-  height: 155px;
+  height: 150px;
   margin-bottom: 0;
   border-radius: 0;
   background: rgb(154, 231, 227);
   color: #FFF;
-  padding: 5px;
+  padding: 10px;
   margin: 10px;
-  border: 5px solid rgb(199, 193, 193);
+  border: 5px solid #CCC;
 }
 
 
@@ -127,12 +86,10 @@ export default {
 #product-component .level-item {
   padding-left: 10px;
   color: #ff0000;
-
 }
 
-#product-component .clickable {
+#product-component .clicavel {
   padding: 0 7.5px;
-  
 }
 </style>
 
