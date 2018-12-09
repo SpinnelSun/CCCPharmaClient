d79799c18e952a0f8cec6f25e3d1995b8aa1fe78
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2018-12-09 20:13:21 -0300

    Create SaleComponent Create component with basic html and css.

---
+++ src/components/sale/SaleComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="sale-component">
+        Pedido nº: {{sale.Id}} <br/>
+        Total: {{sale.Total}}
+        <div id="product-list-Container">
+            <div id="product-list-header">
+                <span id="item-header">Itens</span>
+                <span id="quantity-header">Qtd</span>
+                <span id="price-header">Preço</span>
+            </div>
+            <ul id="product-list">
+                
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SaleComponent',
+    props: {
+      sale: Object
+    }
+}
+</script>
+
+<style>
+
+#sale-component{
+    height: 90%;
+    width: 90%;
+    font-family: 'sans';
+    font-size: 0.875rem;
+    font-weight: 400;
+}
+
+</style>
+
