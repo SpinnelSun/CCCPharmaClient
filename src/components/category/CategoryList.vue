68d8cf6dda4e0994f79352a5a86238ecd3b6581a
SpinnelSun <m.hollygard@gmail.com>
2018-12-10 11:46:14 -0300

    Improve CategoryList appearance

--- src/components/category/CategoryList.vue
+++ src/components/category/CategoryList.vue
@@ -3,11 +3,12 @@
         <div class="list-header">
             <div><strong>Categoria</strong></div>
             <div><strong>Desconto Atual</strong></div>
-            <div><strong>Novo Desconto</strong></div>
+            <div><strong>Novo Desconto?</strong></div>
         </div>
         <div class="list-itens">
             <category-item v-for="category in categories" :key="category.name" v-bind:category="category"></category-item>
         </div>
+        <div class="list-footer"></div>
     </div>
 </template>
 
@@ -19,24 +20,41 @@ export default {
     components: { CategoryItem },
     data() {
         return {
-            categories : [ { name: "Alimentos", discount: 0.00 },
-                           { name: "Cosméticos", discount: 0.10 },
-                           { name: "Higiene Pessoal", discount: 0.25 },
-                           { name: "Medicamentos", discount: 0.50 } ]
+            categories : [ { name: "Alimentos", discount: "0%" },
+                           { name: "Cosméticos", discount: "10%" },
+                           { name: "Higiene Pessoal", discount: "25%" },
+                           { name: "Medicamentos", discount: "50%" } ]
         }
     }
 }
 </script>
  
-<style>
+<style scoped>
     .list-header {
         display: inline-grid;
         justify-content: space-around;
-        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-columns: repeat(3, 1fr);
 
+        border-top-left-radius: 1em;
+        border-top-right-radius: 1em;
+        width: 60em;
+        height: 3em;
+
+        align-content: center;
         text-align: center;
 
         background-color: #242B3A;
         color: #FFFFFF;
+    }
+
+    .list-footer {
+        display: inline-grid;
+
+        border-bottom-left-radius: 1em;
+        border-bottom-right-radius: 1em;
+        width: 60em;
+        height: 1.5em;
+
+        background-color: #242B3A;
     }
 </style>
