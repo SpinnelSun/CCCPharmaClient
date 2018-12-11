b037128132f39e05e5c7c1f6ef440bc12316389a
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-11 10:41:23 -0300

    Create ProductUpdate

---
+++ src/components/product/ProductUpdate.vue
@@ -0,0 +1,43 @@
+<template id = "form-update">
+    <div>
+        <product-form title="Atualizar" :key="product" v-on:handleSubmitProduct="handleSubmitProduct"> </product-form>
+        <span :class="{error: isError}" v-if="isError">
+            <i class="fa fa-warning"></i>
+            Houve um erro na atualização de dados. Reveja suas informações.
+        </span>
+        <span :class="{sucess: isSucess}" v-if="isSucess">
+            <i class="fa fa-check"></i>
+            Produto atualizado com sucesso.
+        </span>
+    </div>
+</template>
+
+
+<script>
+    import { update } from '@/services/productService'
+    import ProductForm from '@/components/product/ProductForm'
+
+    export default {
+        name: 'ProductList',
+
+        data() {
+            return {
+                isError: false,
+                isSucess: false
+            }
+        },
+        components: {
+            ProductForm
+        },
+        methods: {
+            async handleSubmitProduct (product) {
+                const HTTP_STATUS_CREATED = 201;
+                const response = await update(product);
+                this.isError = response.status != HTTP_STATUS_CREATED;
+                this.isSucess = !this.isError;
+            },
+            
+        }
+    }
+
+</script>
