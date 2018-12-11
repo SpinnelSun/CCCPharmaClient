a4eb648414d3a739c71e3397dd3771d224c5eafc
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-10 22:37:38 -0300

    Update ProductList

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -2,7 +2,8 @@
     <div id="app" class="columns">
         <div class="column">
             <ul>
-                <li id="component"> <product-component v-for="product in products" :key="product.id" v-bind:product="product"> </product-component> </li>
+                <!-- add v-on para fillProducts -->
+                <li id="component"> <product-component v-for="product in products" :key="product.code" v-bind:product="product"> </product-component> </li>
             </ul>
         </div>
     </div>
@@ -11,20 +12,24 @@
 
 <script>
     import ProductComponent from '@/components/product/ProductComponent'
-    
 
     export default {
         name: 'ProductList',
         data() {
             return {
-                products : [{name: "camisinha", price: 2.75, id:'1', avaliable: false},
-                {  name: "camisinha",price: 2.85, id:'2', avaliable: true},
-                { name: "camisinha",price: 2.85, id:'3', avaliable: true}, 
-                {name: "camisinha", price: 2.75, id:'4', avaliable: false}]
+                products : [{name: "Camisinha - Sabor Uva", price: 2.75, avaliable: false, producer: "governo", code: "1", amount: 0},
+                {  name: "Camisinha - Chocolate",price: 28.95,  avaliable: true, producer: "Juntex", code: "2", amount: "10"},
+                { name: "Camisinha básica",price: 7.85, avaliable: true, producer: "Governo", code: "3", amount: "30"}, 
+                {name: "Dipirona", price: 4.99,  avaliable: false, producer: "Dipi", code: "4", amount: 0}]
             }
         },
         components: {
             ProductComponent
+        },
+        methods: {
+            async fillProducts(){
+                this.products = await getProducts();
+            }
         }
     }
 
