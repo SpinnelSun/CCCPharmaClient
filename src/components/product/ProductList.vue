9a51ce94841994500488b70f09338b479f0688b4
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-11 10:40:55 -0300

    Add fillProducts on ProductList

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -3,7 +3,7 @@
         <div class="column">
             <ul>
                 <!-- add v-on para fillProducts -->
-                <li id="component"> <product-component v-for="product in products" :key="product.code" v-bind:product="product"> </product-component> </li>
+                <li id="component"> <product-component v-on="fillProducts" v-for="product in products" :key="product.code" > </product-component> </li>
             </ul>
         </div>
     </div>
@@ -12,15 +12,14 @@
 
 <script>
     import ProductComponent from '@/components/product/ProductComponent'
+    import { getProducts } from '@/services/productService'
+
 
     export default {
         name: 'ProductList',
         data() {
             return {
-                products : [{name: "Camisinha - Sabor Uva", price: 2.75, avaliable: false, producer: "governo", code: "1", amount: 0},
-                {  name: "Camisinha - Chocolate",price: 28.95,  avaliable: true, producer: "Juntex", code: "2", amount: "10"},
-                { name: "Camisinha básica",price: 7.85, avaliable: true, producer: "Governo", code: "3", amount: "30"}, 
-                {name: "Dipirona", price: 4.99,  avaliable: false, producer: "Dipi", code: "4", amount: 0}]
+                products : []
             }
         },
         components: {
