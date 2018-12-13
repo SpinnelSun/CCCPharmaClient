26680459ff20743639346c9437e45da1a67f86b6
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-12 22:10:04 -0300

    Create ProductSimpleList

---
+++ src/components/product/ProductSimpleList.vue
@@ -0,0 +1,48 @@
+<template id = "list">
+    <div id="app" class="columns">
+        <div class="column">
+            <ul>
+                <li id="component" v-for="product in products" :key="product.code"> 
+                    <product-simple-component :product=product > </product-simple-component> 
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import ProductSimpleComponent from '@/components/product/ProductSimpleComponent'
+    import { getProducts } from '@/services/productService'
+    
+    export default {
+        name: 'ProductList',
+        data() {
+            return {
+                products : []
+            }
+        },
+        components: {
+            ProductSimpleComponent
+        },
+        async created() {
+            const response = await getProducts();
+            console.log(response.data.content);
+            this.products = response.data.content;
+        }
+    }
+
+</script>
+
+<style>
+    .column ul{
+        padding:0px;
+        margin:0px;
+        background-color:#EDEDED;
+        list-style:none;
+         display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    
+</style>
+
