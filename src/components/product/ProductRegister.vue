e982d1293e37a8269f56ed7a602fc179caf2e44a
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2018-12-12 22:09:18 -0300

    Create ProductRegister

---
+++ src/components/product/ProductRegister.vue
@@ -0,0 +1,45 @@
+<template id = "form-update">
+    <div>
+        <product-form title="Registrar" :key="code" v-on:handleSubmitProduct="handleSubmitProduct"> </product-form>
+        <span :class="{error: isError}" v-if="isError">
+            <i class="fa fa-warning"></i>
+            Houve um erro na atualização de dados. Reveja suas informações.
+        </span>
+        <span :class="{sucess: isSucess}" v-if="isSucess">
+            <i class="fa fa-check"></i>
+            Produto atualizado com sucesso.
+        </span>
+    </div>
+</template>
+
+
+<script>
+    import { save } from '@/services/productService'
+    import ProductForm from '@/components/product/ProductForm'
+
+    export default {
+        name: 'ProductList',
+
+        data() {
+            return {
+                code: this.$route.params.code,
+                product : {},
+                isError: false,
+                isSucess: false
+            }
+        },
+        components: {
+            ProductForm
+        },
+        methods: {
+            async handleSubmitProduct (product) {
+                const HTTP_STATUS_CREATED = 201;
+                const response = await save(product);
+                console.log(response.status);
+                this.isError = response.status != HTTP_STATUS_CREATED;
+                this.isSucess = !this.isError;
+            }, 
+        }
+    }
+
+</script>
