4e90400b01e683d96953a2fb6a78bff5cfdda1e3
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-12-15 18:37:40 -0300

    Add sort products to admin

--- src/components/product/admin/ProductDetailedList.vue
+++ src/components/product/admin/ProductDetailedList.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <div class="products-grid">
+            <div>
+                <span> Listar por: </span>
+                <select @change="sort" class="form-control" v-model="sortingAttribute">
+                    <option value="code">Código</option>
+                    <option value="name">Nome</option>
+                    <option value="price">Preço</option>
+                    <option value="producer">Fabricante</option>
+                    <option value="category">Categoria</option>
+                </select>
+            </div>
             <div class="product-item" v-for="product in products" :key="product.code"> 
                 <product-detailed-item :product=product></product-detailed-item> 
             </div>
@@ -10,13 +20,14 @@
 
 <script>
     import ProductDetailedItem from '@/components/product/admin/ProductDetailedItem'
-    import { getProducts } from '@/services/productService'
+    import { getProducts, getProductsSorted } from '@/services/productService'
     
     export default {
         name: 'ProductDetailedList',
         data() {
             return {
-                products : []
+                products : [],
+                sortingAttribute: ''
             }
         },
         components: {
@@ -25,6 +36,12 @@
         async created() {
             const response = await getProducts();
             this.products = response.data.content;
+        },
+        methods: {
+            async sort () {
+                const response = await getProductsSorted(this.sortingAttribute);
+                this.products = response.data.content;
+            }
         }
     }
 </script>
