104aff5b83b3509ddc1b032a323c42d1a4f820dd
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-12-15 19:06:19 -0300

    Resolve conflicts

--- src/router/routes.js
+++ src/router/routes.js
@@ -22,6 +22,11 @@ const router =  new Router({
       component: Signin
     },
     {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    },
+    {
       path: '/signup',
       name: 'Signup',
       component: UserSignup
