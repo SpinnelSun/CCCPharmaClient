8105e0b49b4e5ef747ec735e86700f4d78e3e8de
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-12-16 17:02:39 -0300

    Refactor axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,17 +2,15 @@ import axios from 'axios';
 
 const API_URL = 'https://cccpharma-server.herokuapp.com';
 
-
 const http = axios.create({
-        baseURL: API_URL,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
-    })
-
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Authorization': `Bearer ${localStorage.token}`
+    }
+})
 
 
 export default http;
