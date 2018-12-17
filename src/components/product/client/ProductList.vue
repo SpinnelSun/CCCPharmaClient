6aaf8b58d268adca2a285e55ab3b881b920af760
SpinnelSun <m.hollygard@gmail.com>
2018-12-16 21:40:08 -0300

    Add minor changes

--- src/components/product/client/ProductList.vue
+++ src/components/product/client/ProductList.vue
@@ -100,8 +100,9 @@
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         grid-template-rows: repeat(3, 1fr);
+        grid-row-gap: 2em;
+
         justify-content: space-around;
-        
         background-color:#FFFFFF;
     }
 
